635e053107d401d14f933bb93888220be54d5f2e
Nikita <nd286@cornell.edu>
2025-03-17 19:17:59 -0400

    improve app.py

--- backend/app.py
+++ backend/app.py
@@ -35,7 +35,7 @@ def json_search(query):
 def home():
     return render_template('base.html',title="sample html")
 
-@app.route("/episodes")
+@app.route("/flavors")
 def episodes_search():
     text = request.args.get("title")
     return json_search(text)

04a28c1484ea898e99cd13ee757ba9e0278d6dfe
Nikita <nd286@cornell.edu>
2025-03-17 17:26:20 -0400

    improve app.py

--- backend/app.py
+++ backend/app.py
@@ -4,16 +4,16 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 import pandas as pd
 
-# ROOT_PATH for linking with all your files. 
+# Set ROOT_PATH for linking with all your files.
 os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 
 # Get the directory of the current script
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
-# Specify the path to the new JSON file relative to the current script
+# Specify the path to the new JSON file with flavors
 json_file_path = os.path.join(current_directory, 'init.json')
 
-# Assuming your new JSON data is stored in a file named 'new_flavors.json'
+# Load the JSON data from the file and create a DataFrame
 with open(json_file_path, 'r') as file:
     data = json.load(file)
     flavors_df = pd.DataFrame(data['flavors'])
@@ -21,20 +21,22 @@ with open(json_file_path, 'r') as file:
 app = Flask(__name__)
 CORS(app)
 
-# Sample search using json with pandas
+# Search function to filter flavors based on a query in the title
 def json_search(query):
+    # Filter rows where 'title' contains the query (case-insensitive)
     matches = flavors_df[flavors_df['title'].str.lower().str.contains(query.lower())]
+    # Select only the columns you want to return in the JSON response
     matches_filtered = matches[['title', 'subhead', 'description', 'rating']]
     matches_filtered_json = matches_filtered.to_json(orient='records')
     return matches_filtered_json
 
 @app.route("/")
 def home():
-    return render_template('base.html', title="sample html")
+    return render_template('base.html', title="Sample HTML")
 
 @app.route("/flavors")
 def flavors_search():
-    text = request.args.get("title")
+    text = request.args.get("title", "")
     return json_search(text)
 
 if 'DB_NAME' not in os.environ:

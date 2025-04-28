b4378864f307fca7f387655fdd39f940051d3c03
Nikita <nd286@cornell.edu>
2025-04-28 11:08:33 -0400

    home button and wish list

--- backend/app.py
+++ backend/app.py
@@ -166,4 +166,4 @@ def add_review():
     return jsonify({"status": "success"})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5001)
